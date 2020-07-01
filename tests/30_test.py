934f2f9d050eeb1c0a18a3e6a6f988500957493a
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-01 16:36:57 -0400

    30.1

---
+++ tests/30_test.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+import subprocess
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        result = subprocess.run(['python', 'blackjack.py'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        self.assertEqual(result,'Your card is:\n', 'Missing print statment.')
+
+if __name__ == '__main__':
+    unittest.main() 
