abf44ac538570c1ff98ecd586e4df104f93c7092
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-09 11:15:05 -0400

    50.1

--- tests/40_test.py
+++ tests/40_test.py
@@ -1,16 +1,16 @@
-import unittest
-import sys
-sys.path.append('./tests')
+# import unittest
+# import sys
+# sys.path.append('./tests')
 
-from utils import *
+# from utils import *
 
-class PythonTest(unittest.TestCase):
-    def test_command(self):
-        self.assertEqual(getLastCommand(), "python blackjack.py", "entered correct last command")
-    def test_cwd(self):
-        self.assertEqual(getCwd(), "/home/strove/project", "entered in correct directory")
+# class PythonTest(unittest.TestCase):
+#     def test_command(self):
+#         self.assertEqual(getLastCommand(), "python blackjack.py", "entered correct last command")
+#     def test_cwd(self):
+#         self.assertEqual(getCwd(), "/home/strove/project", "entered in correct directory")
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
 
     
