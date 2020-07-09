c9ec110549699a051f00755a0821b2b7220c35d0
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-09 16:00:05 -0400

    80.1

---
+++ tests/80_test.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        self.assertEqual(suits, ["spades", "clubs", "hearts", "diamonds"], 'List should be ["spades", "clubs", "hearts", "diamonds"]')
+if __name__ == '__main__':
+    unittest.main() 
