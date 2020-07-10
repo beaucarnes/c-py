93e306150fc7383f977dab79c9ae5f1db00fec50
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 08:38:41 -0400

    120.1S

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,3 @@
 suits = ["spades", "clubs", "hearts", "diamonds"]
-suit = suits[1]
-rank = "K"
-value = 10
-print("Your card is: " + rank + " of " + suit)
-suits.append("snakes")
 for suit in suits:
     print(suit)
