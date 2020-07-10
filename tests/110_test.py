f72d1fdc216bd4a18869d64389abbcf31fd1a1a1
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 08:37:49 -0400

    120.1

--- tests/110_test.py
+++ tests/110_test.py
@@ -1,11 +1,11 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        self.assertEqual(suits, ["spades", "clubs", "hearts", "diamonds", "snakes"], 'The suits variable should be ["spades", "clubs", "hearts", "diamonds", "snakes"]')
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         self.assertEqual(suits, ["spades", "clubs", "hearts", "diamonds", "snakes"], 'The suits variable should be ["spades", "clubs", "hearts", "diamonds", "snakes"]')
+# if __name__ == '__main__':
+#     unittest.main() 
