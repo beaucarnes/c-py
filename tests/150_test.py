f9e5885f057e7361113537128aced631335dfaaa
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 13:56:41 -0400

    150.1

---
+++ tests/150_test.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+import subprocess
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        result = subprocess.run(['python', 'blackjack.py'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        self.assertEqual(result,"['spades', 'A']\n['clubs', 'A']\n['hearts', 'A']\n['diamonds', 'A']\n", 'Should print 4 lists.')
+
+if __name__ == '__main__':
+    unittest.main() 
