9d8a8ee52d79769193755dccf9e7d42b3e70bea1
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 08:44:50 -0400

    140.1

--- tests/130_test.py
+++ tests/130_test.py
@@ -1,11 +1,11 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        self.assertEqual(ranks, ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"], 'The ranks variable should be ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]')
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         self.assertEqual(ranks, ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"], 'The ranks variable should be ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]')
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/140_test.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        self.assertTrue(len(cards) == 0, 'Cards should be an empty list.')
+if __name__ == '__main__':
+    unittest.main() 
