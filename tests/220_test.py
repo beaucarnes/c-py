b8422f7b1af9b739962a91168e14123c336fff66
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 15:38:00 -0400

    230.1

--- tests/220_test.py
+++ tests/220_test.py
@@ -1,13 +1,13 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        self.assertEqual(len(card), 2, "card should be a random 2-item list from cards list.")
-    def test_python2(self):
-        self.assertEqual(len(cards), 51, "cards should have 51 items.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         self.assertEqual(len(card), 2, "card should be a random 2-item list from cards list.")
+#     def test_python2(self):
+#         self.assertEqual(len(cards), 51, "cards should have 51 items.")
+# if __name__ == '__main__':
+#     unittest.main() 
