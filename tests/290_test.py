795e9dec1916ce102b18dad132d0cef4afe2a517
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 16:10:51 -0400

    300.1

--- tests/290_test.py
+++ tests/290_test.py
@@ -1,13 +1,13 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        result = deal(2)
-        self.assertEqual(len(result), 0, 'deal function should return and empty list')
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         result = deal(2)
+#         self.assertEqual(len(result), 0, 'deal function should return and empty list')
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
