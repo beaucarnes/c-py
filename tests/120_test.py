9931d0574aba3b529dea6813eb842a533639e3df
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 08:40:38 -0400

    130.1

--- tests/120_test.py
+++ tests/120_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        variables_exist = 'rank' in globals() or 'value' in globals()
-        self.assertFalse(variables_exist, 'The rank and value variables should be removed.')
-    def test_python2(self):
-        self.assertEqual(suits, ["spades", "clubs", "hearts", "diamonds"], 'The suits variable should be ["spades", "clubs", "hearts", "diamonds"]')
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         variables_exist = 'rank' in globals() or 'value' in globals()
+#         self.assertFalse(variables_exist, 'The rank and value variables should be removed.')
+#     def test_python2(self):
+#         self.assertEqual(suits, ["spades", "clubs", "hearts", "diamonds"], 'The suits variable should be ["spades", "clubs", "hearts", "diamonds"]')
+# if __name__ == '__main__':
+#     unittest.main() 
