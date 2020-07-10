3d35ad8e55b274e154ebba85cc8ebefbdc299304
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-10 15:57:07 -0400

    260.1

---
+++ tests/260_test.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        card_delt = deal()
+        self.assertEqual(len(card_delt), 2, 'card_delt should be a list of length 2.')
+    def test_python2(self):
+        self.assertEqual(len(cards), 51, 'cards should be a list of length 51 after deal function is called.')
+
+if __name__ == '__main__':
+    unittest.main() 
