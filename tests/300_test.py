03a9ae524ac99d4dfdcc735f7f9b2aba5761ea19
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-13 12:41:23 -0400

    320.1

--- tests/300_test.py
+++ tests/300_test.py
@@ -1,15 +1,15 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        result = deal(3)
-        self.assertEqual(len(result), 3, 'Calling deal with argument 3 should return list of length 3.')
-    def test_python2(self):
-        self.assertEqual(len(cards), 47, 'cards should be down to 47 items.')
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         result = deal(3)
+#         self.assertEqual(len(result), 3, 'Calling deal with argument 3 should return list of length 3.')
+#     def test_python2(self):
+#         self.assertEqual(len(cards), 47, 'cards should be down to 47 items.')
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/320_test.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+sys.path.append('..')
+import random
+random.seed(15)
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        self.assertEqual(rank, card[1], "rank should be rank from card")
+
+if __name__ == '__main__':
+    unittest.main() 
