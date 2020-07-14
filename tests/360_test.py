6f1d9d1e8a5b54b62f1d5bca8adf6cc4cb8361be
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-14 12:59:08 -0400

    370.1

--- tests/360_test.py
+++ tests/360_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-sys.path.append('..')
-import random
-random.seed(1)
+# import unittest
+# import sys
+# sys.path.append('..')
+# import random
+# random.seed(1)
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        self.assertEqual(value, "9", "value should be 9 if rank is 9.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         self.assertEqual(value, "9", "value should be 9 if rank is 9.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/370_test.py
@@ -0,0 +1,16 @@
+import unittest
+import subprocess
+import sys
+sys.path.append('..')
+import random
+random.seed(1)
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python2(self):
+        count_or = open("blackjack.py").read().count("print(rank, value)")
+        self.assertEqual(count_or, 1, "Code should contain print(rank, value).")
+
+if __name__ == '__main__':
+    unittest.main() 
