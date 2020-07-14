29c78ff7341b2dd1ce105c5b3dd12828d818d581
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-14 13:48:50 -0400

    400.1

--- tests/390_test.py
+++ tests/390_test.py
@@ -1,19 +1,19 @@
-import unittest
-import subprocess
-import sys
-import re
-sys.path.append('..')
-import random
-random.seed(1)
+# import unittest
+# import subprocess
+# import sys
+# import re
+# sys.path.append('..')
+# import random
+# random.seed(1)
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python2(self):
-        code = open("blackjack.py").read()
-        pattern = re.compile("rank_dict\[[\"\']rank[\"\']\]\,\s?rank_dict\[[\"\']value[\"\']\]")
-        found_pattern = pattern.search(code)
-        self.assertTrue(found_pattern, "Code print items from dictionary.")
+# class PythonTest(unittest.TestCase):
+#     def test_python2(self):
+#         code = open("blackjack.py").read()
+#         pattern = re.compile("rank_dict\[[\"\']rank[\"\']\]\,\s?rank_dict\[[\"\']value[\"\']\]")
+#         found_pattern = pattern.search(code)
+#         self.assertTrue(found_pattern, "Code print items from dictionary.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
