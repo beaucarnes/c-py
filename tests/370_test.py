e112a41f8ba89083560b40f158662c79e5cc6d22
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-14 13:11:12 -0400

    380.1

--- tests/370_test.py
+++ tests/370_test.py
@@ -1,16 +1,16 @@
-import unittest
-import subprocess
-import sys
-sys.path.append('..')
-import random
-random.seed(1)
+# import unittest
+# import subprocess
+# import sys
+# sys.path.append('..')
+# import random
+# random.seed(1)
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python2(self):
-        count_or = open("blackjack.py").read().count("print(rank, value)")
-        self.assertEqual(count_or, 1, "Code should contain print(rank, value).")
+# class PythonTest(unittest.TestCase):
+#     def test_python2(self):
+#         count_or = open("blackjack.py").read().count("print(rank, value)")
+#         self.assertEqual(count_or, 1, "Code should contain print(rank, value).")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/380_test.py
@@ -0,0 +1,15 @@
+import unittest
+import subprocess
+import sys
+sys.path.append('..')
+import random
+random.seed(1)
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python2(self):
+        self.assertEqual(rank_dict, {"rank": "9", "value": "9"}, "rank_dict should be correct dictionary.")
+
+if __name__ == '__main__':
+    unittest.main() 
