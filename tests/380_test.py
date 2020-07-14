1379ea3c1aa35789aa18453a2c30f53ed10cd222
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-14 13:41:08 -0400

    390.1

--- tests/380_test.py
+++ tests/380_test.py
@@ -1,15 +1,15 @@
-import unittest
-import subprocess
-import sys
-sys.path.append('..')
-import random
-random.seed(1)
+# import unittest
+# import subprocess
+# import sys
+# sys.path.append('..')
+# import random
+# random.seed(1)
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python2(self):
-        self.assertEqual(rank_dict, {"rank": "9", "value": "9"}, "rank_dict should be correct dictionary.")
+# class PythonTest(unittest.TestCase):
+#     def test_python2(self):
+#         self.assertEqual(rank_dict, {"rank": "9", "value": "9"}, "rank_dict should be correct dictionary.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
