bb6bf4f2a04ab1da2e3a4d4f94f12485ea6d6e05
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-14 11:39:26 -0400

    360.1

--- tests/350_test.py
+++ tests/350_test.py
@@ -1,20 +1,20 @@
-import unittest
-import sys
-sys.path.append('..')
-import random
-random.seed(314)
+# import unittest
+# import sys
+# sys.path.append('..')
+# import random
+# random.seed(314)
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
+# class PythonTest(unittest.TestCase):
 
-    def test_python(self):
-        self.assertEqual(value, 10, "value should be 10 if rank is 'K'.")
+#     def test_python(self):
+#         self.assertEqual(value, 10, "value should be 10 if rank is 'K'.")
 
-    def test_python2(self):
-        count_or = open("blackjack.py").read().count(" or ")
-        print("!!", count_or)
-        self.assertEqual(count_or, 2, "Code should contain two or statements.")
+    # def test_python2(self):
+    #     count_or = open("blackjack.py").read().count(" or ")
+    #     print("!!", count_or)
+    #     self.assertEqual(count_or, 2, "Code should contain two or statements.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
