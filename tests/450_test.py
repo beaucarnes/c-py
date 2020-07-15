e02a692d9ec658ffd05ed9ee9c96bcb7e16a85ae
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-15 16:08:32 -0400

    460.1

--- tests/450_test.py
+++ tests/450_test.py
@@ -1,21 +1,21 @@
-import unittest
-import sys
-import re
-sys.path.append('..')
+# import unittest
+# import sys
+# import re
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        deck_instance = Deck()
-        correct_ranks = [{'rank': 'A', 'value': 11}, {'rank': '2', 'value': 2}, {'rank': '3', 'value': 3}, {'rank': '4', 'value': 4}, {'rank': '5', 'value': 5}, {'rank': '6', 'value': 6}, {'rank': '7', 'value': 7}, {'rank': '8', 'value': 8}, {'rank': '9', 'value': 9}, {'rank': '10', 'value': 10}, {'rank': 'J', 'value': 10}, {'rank': 'Q', 'value': 10}, {'rank': 'K', 'value': 10}]
-        self.assertEqual(deck_instance.ranks, correct_ranks, "There should be a Deck class.")
-    def test_python2(self):
-        self.assertFalse('card' in locals(), "card variable should not exist.")
-    def test_python3(self):
-        code = open("blackjack.py").read()
-        pattern = re.compile("print\(card\[1\]\[[\"\']value[\"\']\]\)")
-        found_pattern = pattern.search(code)
-        self.assertFalse(found_pattern, "Code should not print card value.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         deck_instance = Deck()
+#         correct_ranks = [{'rank': 'A', 'value': 11}, {'rank': '2', 'value': 2}, {'rank': '3', 'value': 3}, {'rank': '4', 'value': 4}, {'rank': '5', 'value': 5}, {'rank': '6', 'value': 6}, {'rank': '7', 'value': 7}, {'rank': '8', 'value': 8}, {'rank': '9', 'value': 9}, {'rank': '10', 'value': 10}, {'rank': 'J', 'value': 10}, {'rank': 'Q', 'value': 10}, {'rank': 'K', 'value': 10}]
+#         self.assertEqual(deck_instance.ranks, correct_ranks, "There should be a Deck class.")
+#     def test_python2(self):
+#         self.assertFalse('card' in locals(), "card variable should not exist.")
+#     def test_python3(self):
+#         code = open("blackjack.py").read()
+#         pattern = re.compile("print\(card\[1\]\[[\"\']value[\"\']\]\)")
+#         found_pattern = pattern.search(code)
+#         self.assertFalse(found_pattern, "Code should not print card value.")
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/460_test.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python1(self):
+        text_found = open("blackjack.py").read().find("def __init__(self):")
+        self.assertTrue(text_found > 0, "Code should contain 'def __init__(self):'.")
+    def test_python2(self):
+        deck = Deck()
+        self.assertFalse(hasattr(deck, 'ranks'), "ranks should be in __init__ method")
+
+if __name__ == '__main__':
+    unittest.main() 
