3884c85f7c3db8f16f24704ae9da7b6cabee5790
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-15 16:31:31 -0400

    500.1

--- tests/480_test.py
+++ tests/480_test.py
@@ -1,15 +1,15 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python1(self):
-        deck = Deck()
-        deck.shuffle()
-        cards = deck.deal(3)
-        self.assertEqual(len(cards), 3, "All Deck methods should work and deal(3) should return a list of length 3.")
+# class PythonTest(unittest.TestCase):
+#     def test_python1(self):
+#         deck = Deck()
+#         deck.shuffle()
+#         cards = deck.deal(3)
+#         self.assertEqual(len(cards), 3, "All Deck methods should work and deal(3) should return a list of length 3.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
--- tests/490_test.py
+++ tests/490_test.py
@@ -1,12 +1,12 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        self.assertTrue(isinstance(deck1, Deck), "deck1 should be instance of Deck.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         self.assertTrue(isinstance(deck1, Deck), "deck1 should be instance of Deck.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
