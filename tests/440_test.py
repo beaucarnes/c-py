1ebd3ae02cdc780b347d20d6b81e1f59449b1201
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-15 15:36:20 -0400

    440.1

---
+++ tests/440_test.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+import re
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python1(self):
+        code = open("blackjack.py").read()
+        pattern = re.compile("print\(card\[1\]\[[\"\']value[\"\']\]\)")
+        found_pattern = pattern.search(code)
+        self.assertTrue(found_pattern, "Code prints card value.")
+
+if __name__ == '__main__':
+    unittest.main() 
