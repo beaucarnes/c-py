d0216c294e859cb9d2020eca460b1ed720ad520d
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-15 15:30:58 -0400

    430.1

--- tests/420_test.py
+++ tests/420_test.py
@@ -1,27 +1,27 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python1(self):
-        correct_ranks = ranks = [
-            {"rank": "A", "value": 11},
-            {"rank": "2", "value": 2},
-            {"rank": "3", "value": 3},
-            {"rank": "4", "value": 4},
-            {"rank": "5", "value": 5},
-            {"rank": "6", "value": 6},
-            {"rank": "7", "value": 7},
-            {"rank": "8", "value": 8},
-            {"rank": "9", "value": 9},
-            {"rank": "10", "value": 10},
-            {"rank": "J", "value": 10},
-            {"rank": "Q", "value": 10},
-            {"rank": "K", "value": 10},
-        ]
-        self.assertEqual(ranks, correct_ranks, "ranks should equal a list of dictionaries.")
+# class PythonTest(unittest.TestCase):
+#     def test_python1(self):
+#         correct_ranks = ranks = [
+#             {"rank": "A", "value": 11},
+#             {"rank": "2", "value": 2},
+#             {"rank": "3", "value": 3},
+#             {"rank": "4", "value": 4},
+#             {"rank": "5", "value": 5},
+#             {"rank": "6", "value": 6},
+#             {"rank": "7", "value": 7},
+#             {"rank": "8", "value": 8},
+#             {"rank": "9", "value": 9},
+#             {"rank": "10", "value": 10},
+#             {"rank": "J", "value": 10},
+#             {"rank": "Q", "value": 10},
+#             {"rank": "K", "value": 10},
+#         ]
+#         self.assertEqual(ranks, correct_ranks, "ranks should equal a list of dictionaries.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
