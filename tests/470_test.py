165e792cd71455c60c33ec02ef346984a619ef6a
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-15 16:13:44 -0400

    470.1

---
+++ tests/470_test.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python1(self):
+        text_found = open("blackjack.py").read().find("shuffle(self)")
+        self.assertTrue(text_found > 0, "Code should contain 'shuffle(self)'.")
+    def test_python2(self):
+        text_found = open("blackjack.py").read().find("deal(self,")
+        self.assertTrue(text_found > 0, "Code should contain 'deal(self,'.")
+
+if __name__ == '__main__':
+    unittest.main() 
