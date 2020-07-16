4c9216a3b0c1b05a20a44dc12794ad0713201742
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-16 14:12:14 -0400

    570.1

--- tests/560_test.py
+++ tests/560_test.py
@@ -1,13 +1,13 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        card = Card()
-        self.assertEqual(card.suit, "hearts", "card.suit should be 'hearts'")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         card = Card()
+#         self.assertEqual(card.suit, "hearts", "card.suit should be 'hearts'")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/570_test.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        card = Card()
+        self.assertEqual(card.rank, "A", "card.rank should be 'A'")
+
+if __name__ == '__main__':
+    unittest.main() 
