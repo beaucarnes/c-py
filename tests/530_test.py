3d3c17c22f32379dc24d7dfd44500a1f425b6f15
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-16 13:17:27 -0400

    540.1

--- tests/530_test.py
+++ tests/530_test.py
@@ -1,13 +1,13 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        text_found = open("blackjack.py").read().find("print(deck2.cards)")
-        self.assertTrue(text_found > 0, "Code should contain 'print(deck2.cards)'.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         text_found = open("blackjack.py").read().find("print(deck2.cards)")
+#         self.assertTrue(text_found > 0, "Code should contain 'print(deck2.cards)'.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
