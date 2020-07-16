39e6780407bb2fa7b13296676f8eb74351e0db32
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-16 14:32:17 -0400

    600.1

--- tests/590_test.py
+++ tests/590_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        card1 = Card("diamonds", {"rank": "A", "value": 11})
-        text = card1.__str__()
-        self.assertEqual(text, "A of diamonds", "__str__ should be defined correctly")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         card1 = Card("diamonds", {"rank": "A", "value": 11})
+#         text = card1.__str__()
+#         self.assertEqual(text, "A of diamonds", "__str__ should be defined correctly")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/600_test.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+import subprocess
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        result = subprocess.run(['python', 'blackjack.py'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        self.assertEqual(result,'A of diamonds\n5 of spades\n', 'Should have code to create and print cards.')
+
+if __name__ == '__main__':
+    unittest.main() 
