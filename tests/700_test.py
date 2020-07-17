6060bc13733b6da1d427a066c13256c327c4f06c
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-17 14:02:07 -0400

    710.1

--- tests/700_test.py
+++ tests/700_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        hand.value = 9
-        hand.calculate_value()
-        self.assertEqual(hand.value, 0, "hand.calculate_value() should set value to 0.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         hand.value = 9
+#         hand.calculate_value()
+#         self.assertEqual(hand.value, 0, "hand.calculate_value() should set value to 0.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
