8b404547ca2b0a07e3dd637b06d2dfd34e255b9f
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-17 13:51:05 -0400

    690.1

--- tests/680_test.py
+++ tests/680_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        hand = Hand(True)
-        hand.add_card([1, 2, 3])
-        self.assertEqual(len(hand.cards), 3, "add_card method should add elements to cards.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         hand = Hand(True)
+#         hand.add_card([1, 2, 3])
+#         self.assertEqual(len(hand.cards), 3, "add_card method should add elements to cards.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
