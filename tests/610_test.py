0e7276a05f956d5dbfd0571950079d138c4098ea
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-17 10:52:37 -0400

    620.1

--- tests/610_test.py
+++ tests/610_test.py
@@ -1,23 +1,23 @@
-import unittest
-import sys
-sys.path.append('..')
-import re
+# import unittest
+# import sys
+# sys.path.append('..')
+# import re
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        card1 = Card("diamonds", {"rank": "A", "value": 11})
-        text = card1.__str__()
-        self.assertEqual(text, "A of diamonds", "__str__ should return the correct string.")
-    def test_python2(self):
-        code = open("blackjack.py").read()
-        pattern = re.compile("f[\'\"]")
-        found_pattern = pattern.search(code)
-        self.assertTrue(found_pattern, "Code should contain f string.")
-    def test_python3(self):
-        text_found = open("blackjack.py").read().find('" of " + self.suit')
-        self.assertFalse(text_found > 0, '''Code should not contain '" of " + self.suit'.''')
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         card1 = Card("diamonds", {"rank": "A", "value": 11})
+#         text = card1.__str__()
+#         self.assertEqual(text, "A of diamonds", "__str__ should return the correct string.")
+#     def test_python2(self):
+#         code = open("blackjack.py").read()
+#         pattern = re.compile("f[\'\"]")
+#         found_pattern = pattern.search(code)
+#         self.assertTrue(found_pattern, "Code should contain f string.")
+#     def test_python3(self):
+#         text_found = open("blackjack.py").read().find('" of " + self.suit')
+#         self.assertFalse(text_found > 0, "Code should not contain '" of " + self.suit'.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/620_test.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        deck = Deck()
+        self.assertTrue(isinstance(deck.cards[10], Card), "Cards in a deck should be instances of Card.")
+
+if __name__ == '__main__':
+    unittest.main() 
