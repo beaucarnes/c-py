485f218ca65dea2b1468e0d80f28adc2fdca229d
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-17 10:43:52 -0400

    610.1

--- tests/600_test.py
+++ tests/600_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-import subprocess
-sys.path.append('..')
+# import unittest
+# import sys
+# import subprocess
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        result = subprocess.run(['python', 'blackjack.py'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        self.assertEqual(result,'A of diamonds\n5 of spades\n', 'Should have code to create and print cards.')
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         result = subprocess.run(['python', 'blackjack.py'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+#         self.assertEqual(result,'A of diamonds\n5 of spades\n', 'Should have code to create and print cards.')
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
