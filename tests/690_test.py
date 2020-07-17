6a18479a6b9fa9d339718649509abcc6d4bac82c
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-17 13:55:58 -0400

    700.1

--- tests/690_test.py
+++ tests/690_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        text_found = open("blackjack.py").read().find('print(hand.cards)')
-        self.assertTrue(text_found > 0, "Code should contain 'print(hand.cards)'.")
-    def test_python2(self):
-        self.assertEqual(len(hand.cards), 2, "hand should contain 2 cards after adding code.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         text_found = open("blackjack.py").read().find('print(hand.cards)')
+#         self.assertTrue(text_found > 0, "Code should contain 'print(hand.cards)'.")
+#     def test_python2(self):
+#         self.assertEqual(len(hand.cards), 2, "hand should contain 2 cards after adding code.")
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/700_test.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        hand.value = 9
+        hand.calculate_value()
+        self.assertEqual(hand.value, 0, "hand.calculate_value() should set value to 0.")
+
+if __name__ == '__main__':
+    unittest.main() 
