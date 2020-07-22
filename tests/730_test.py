2382ab85c42a3be13012fb63323be102641b1136
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-22 14:45:57 -0400

    730.1

---
+++ tests/730_test.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+import re
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        text_found = open("blackjack.py").read().find('+=')
+        self.assertTrue(text_found > 0, "Code should contain '+='.")
+    def test_python2(self):
+        hand2 = Hand()
+        hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
+        hand2.add_card([Card('hearts', {"rank": "8", "value": 8})])
+        hand2.calculate_value()
+        self.assertEqual(hand2.value, 19, "calculate_value() should work correctly.")
+if __name__ == '__main__':
+    unittest.main() 
