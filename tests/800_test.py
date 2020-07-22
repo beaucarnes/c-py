428163f4093e4ce9aaaefba2121bb511f8971c72
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-22 15:37:10 -0400

    810.1

--- tests/800_test.py
+++ tests/800_test.py
@@ -1,15 +1,15 @@
-import unittest
-import sys
-import re
-sys.path.append('..')
+# import unittest
+# import sys
+# import re
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        code = open("blackjack.py").read()
-        pattern = re.compile("print\(f'''{\"Dealer's\" if self.dealer else \"Your\"} hand:'''\)")
-        found_pattern = pattern.search(code)
-        self.assertTrue(found_pattern, "Use triple quotes with specific code string.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         code = open("blackjack.py").read()
+#         pattern = re.compile("print\(f'''{\"Dealer's\" if self.dealer else \"Your\"} hand:'''\)")
+#         found_pattern = pattern.search(code)
+#         self.assertTrue(found_pattern, "Use triple quotes with specific code string.")
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/810_test.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+import re
+import io
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+
+        hand2 = Hand(True)
+        hand2.add_card([Card('spades', {"rank": "K", "value": 10})])
+        hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        hand2.display()                                 # Call function.
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        self.assertEqual(capturedOutput.getvalue(), "Dealer's hand:\nK of spades\nA of hearts\n", "display() should print correct string.")
+if __name__ == '__main__':
+    unittest.main() 
