2e77dc36944c0ad26d7760837620bb0afed75a01
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-22 15:50:36 -0400

    840.1

--- tests/830_test.py
+++ tests/830_test.py
@@ -1,20 +1,20 @@
-import unittest
-import sys
-import re
-import io
-sys.path.append('..')
+# import unittest
+# import sys
+# import re
+# import io
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        hand2 = Hand()
-        hand2.add_card([Card('spades', {"rank": "K", "value": 10})])
-        hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
-        capturedOutput = io.StringIO()                  # Create StringIO object
-        sys.stdout = capturedOutput                     #  and redirect stdout.
-        hand2.display()                                 # Call function.
-        sys.stdout = sys.__stdout__                     # Reset redirect.
-        self.assertEqual(capturedOutput.getvalue(), "Your hand:\nK of spades\nA of hearts\nValue: 21\n\n", "display() should print correct string when not dealer.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         hand2 = Hand()
+#         hand2.add_card([Card('spades', {"rank": "K", "value": 10})])
+#         hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
+#         capturedOutput = io.StringIO()                  # Create StringIO object
+#         sys.stdout = capturedOutput                     #  and redirect stdout.
+#         hand2.display()                                 # Call function.
+#         sys.stdout = sys.__stdout__                     # Reset redirect.
+#         self.assertEqual(capturedOutput.getvalue(), "Your hand:\nK of spades\nA of hearts\nValue: 21\n\n", "display() should print correct string when not dealer.")
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/840_test.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+sys.path.append('..')
+import re
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        text_found = open("blackjack.py").read().find('print(hand.display())')
+        self.assertFalse(text_found > 0, "Code should not contain 'print(hand.display())'.")
+
+if __name__ == '__main__':
+    unittest.main() 
