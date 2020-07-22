732782ccc0fc54023743d42046b11bc305ff0695
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-22 14:58:28 -0400

    750.1

--- tests/740_test.py
+++ tests/740_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-import re
-sys.path.append('..')
+# import unittest
+# import sys
+# import re
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        text_found = open("blackjack.py").read().find('has_ace = False')
-        self.assertTrue(text_found > 0, "Code should contain 'has_ace = False'.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         text_found = open("blackjack.py").read().find('has_ace = False')
+#         self.assertTrue(text_found > 0, "Code should contain 'has_ace = False'.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
