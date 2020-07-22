fb75f1b54b5b845cf5a3cf6ee9d2179693e519b6
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-22 15:19:44 -0400

    780.1

--- tests/770_test.py
+++ tests/770_test.py
@@ -1,16 +1,16 @@
-import unittest
-import sys
-import re
-sys.path.append('..')
+# import unittest
+# import sys
+# import re
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        hand2 = Hand()
-        hand2.add_card([Card('spades', {"rank": "9", "value": 9})])
-        hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
-        hand2.add_card([Card('hearts', {"rank": "8", "value": 8})])
-        self.assertEqual(hand2.get_value(), 18, "get_value() should return the hand's value.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         hand2 = Hand()
+#         hand2.add_card([Card('spades', {"rank": "9", "value": 9})])
+#         hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
+#         hand2.add_card([Card('hearts', {"rank": "8", "value": 8})])
+#         self.assertEqual(hand2.get_value(), 18, "get_value() should return the hand's value.")
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/780_test.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+import re
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        hand2 = Hand()
+        hand2.add_card([Card('spades', {"rank": "K", "value": 10})])
+        hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
+        self.assertTrue(hand2.is_blackjack(), "is_blackjack() should return True if blackjack.")
+if __name__ == '__main__':
+    unittest.main() 
