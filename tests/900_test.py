8ddb9752fdcb9a5ad7a3e3438102bddcec4d9b9c
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-23 14:18:21 -0400

    910.1

--- tests/900_test.py
+++ tests/900_test.py
@@ -1,12 +1,12 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        variables_exist = 'deck' in globals() or 'hand' in globals()
-        self.assertFalse(variables_exist, 'The deck and hand variables should be removed.')
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         variables_exist = 'deck' in globals() or 'hand' in globals()
+#         self.assertFalse(variables_exist, 'The deck and hand variables should be removed.')
+# if __name__ == '__main__':
+#     unittest.main() 
