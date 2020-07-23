84c41efb884662768be3c1d9d4bbfb3e3df78682
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-23 10:40:26 -0400

    880.1

--- tests/870_test.py
+++ tests/870_test.py
@@ -1,17 +1,17 @@
-import unittest
-import sys
-sys.path.append('..')
-import re
+# import unittest
+# import sys
+# sys.path.append('..')
+# import re
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        hand = Hand()
-        hand.display(show_all_dealer_cards=True)
-        text = open("blackjack.py").read()
-        text_found = text.find('show_all_dealer_cards=False') or text.find('show_all_dealer_cards = False')
-        self.assertTrue(text_found > 0, "Code should contain 'show_all_dealer_cards=False'.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         hand = Hand()
+#         hand.display(show_all_dealer_cards=True)
+#         text = open("blackjack.py").read()
+#         text_found = text.find('show_all_dealer_cards=False') or text.find('show_all_dealer_cards = False')
+#         self.assertTrue(text_found > 0, "Code should contain 'show_all_dealer_cards=False'.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/880_test.py
@@ -0,0 +1,29 @@
+import unittest
+import sys
+import re
+import io
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        hand2 = Hand(True)
+        hand2.add_card([Card('spades', {"rank": "K", "value": 10})])
+        hand2.add_card([Card('hearts', {"rank": "A", "value": 11})])
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        hand2.display(show_all_dealer_cards=True)                                 # Call function.
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        self.assertEqual(capturedOutput.getvalue(), "Dealer's hand:\nK of spades\nA of hearts\n\n", "display() should print correct string when dealer.")
+    def test_python2(self):
+        hand3 = Hand(True)
+        hand3.add_card([Card('spades', {"rank": "K", "value": 10})])
+        hand3.add_card([Card('hearts', {"rank": "A", "value": 11})])
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        hand3.display()                                 # Call function.
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        self.assertEqual(capturedOutput.getvalue(), "Dealer's hand:\nhidden\nA of hearts\n\n", "display() should print correct string when dealer.")
+if __name__ == '__main__':
+    unittest.main() 
