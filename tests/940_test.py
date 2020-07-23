0bb4939b034e82f7a7cd4035df2524206c0d5b72
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-23 14:33:00 -0400

    950.1

--- tests/940_test.py
+++ tests/940_test.py
@@ -1,13 +1,13 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        text = open("blackjack.py").read()
-        text_found = text.find('int(input("How many games do you want to play? "))')
-        self.assertTrue(text_found > 0, "Code should case input to int.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         text = open("blackjack.py").read()
+#         text_found = text.find('int(input("How many games do you want to play? "))')
+#         self.assertTrue(text_found > 0, "Code should case input to int.")
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/950_test.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+import inspect
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+    def test_python(self):
+        text = open("blackjack.py").read()
+        text_found = text.find('g.play()')
+        self.assertTrue(text_found > 0, "Code should have text 'g.play()'.")
+    def test_python2(self):
+        self.assertTrue(isinstance(g, Game) "g should be instance of Game.")
+if __name__ == '__main__':
+    unittest.main() 
