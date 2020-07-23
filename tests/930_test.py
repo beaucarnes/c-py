b613caccb5364b2c461b6629967124fd0b098caf
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-23 14:27:47 -0400

    940.1

--- tests/930_test.py
+++ tests/930_test.py
@@ -1,13 +1,13 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        text = open("blackjack.py").read()
-        text_found = text.find('games_to_play = input("How many games do you want to play? ")')
-        self.assertTrue(text_found > 0, "Code should contain correct input method.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         text = open("blackjack.py").read()
+#         text_found = text.find('games_to_play = input("How many games do you want to play? ")')
+#         self.assertTrue(text_found > 0, "Code should contain correct input method.")
+# if __name__ == '__main__':
+#     unittest.main() 
