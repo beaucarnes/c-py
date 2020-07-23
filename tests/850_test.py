9afca0eec244ee6214bbcd320727ec3ad1bd2aa0
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-23 10:27:24 -0400

    860.1

--- tests/850_test.py
+++ tests/850_test.py
@@ -1,14 +1,14 @@
-import unittest
-import sys
-sys.path.append('..')
-import re
+# import unittest
+# import sys
+# sys.path.append('..')
+# import re
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        text_found = open("blackjack.py").read().find('for index, card in enumerate(self.cards):')
-        self.assertTrue(text_found > 0, "Code should contain 'for index, card in enumerate(self.cards):'.")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         text_found = open("blackjack.py").read().find('for index, card in enumerate(self.cards):')
+#         self.assertTrue(text_found > 0, "Code should contain 'for index, card in enumerate(self.cards):'.")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
