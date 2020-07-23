40984e3c6422d8fd1546a442256952544cdcdf2e
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-23 14:20:16 -0400

    920.1

--- tests/910_test.py
+++ tests/910_test.py
@@ -1,15 +1,15 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        game = Game()
-        game.play()
-        text = open("blackjack.py").read()
-        text_found = text.find('game_number = 0') or text.find('game_number=0')
-        self.assertTrue(text_found > 0, "Code should contain Game class with play method and 'game_number = 0'.")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         game = Game()
+#         game.play()
+#         text = open("blackjack.py").read()
+#         text_found = text.find('game_number = 0') or text.find('game_number=0')
+#         self.assertTrue(text_found > 0, "Code should contain Game class with play method and 'game_number = 0'.")
+# if __name__ == '__main__':
+#     unittest.main() 
