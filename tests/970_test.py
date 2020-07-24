14ae316d061afbef39ebb9aee3eeb17e2acaea7d
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-24 15:33:23 -0400

    980.1

--- tests/970_test.py
+++ tests/970_test.py
@@ -1,23 +1,23 @@
-import unittest
-from unittest import mock
-import sys
-sys.path.append('..')
+# import unittest
+# from unittest import mock
+# import sys
+# sys.path.append('..')
 
-class PythonTest(unittest.TestCase):
+# class PythonTest(unittest.TestCase):
 
-    def test_python(self):
-        code = open("blackjack.py").read()
-        code = code.replace('g = Game()', '        return locals()\ng = Game()')
-        with unittest.mock.patch('builtins.input', side_effect=[1, "f", 'h', 9]): # side_effects is the list of mocked inputs
-            exec(code)
-            g = locals()["Game"]()
-            local_variables = g.play()
+#     def test_python(self):
+#         code = open("blackjack.py").read()
+#         code = code.replace('g = Game()', '        return locals()\ng = Game()')
+#         with unittest.mock.patch('builtins.input', side_effect=[1, "f", 'h', 9]): # side_effects is the list of mocked inputs
+#             exec(code)
+#             g = locals()["Game"]()
+#             local_variables = g.play()
 
-        self.assertEqual(local_variables['games_to_play'], 9)
+#         self.assertEqual(local_variables['games_to_play'], 9)
 
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
 
 
 
