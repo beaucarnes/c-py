edad40e0e201457ebfa99611d4e237e92072794c
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-24 15:27:37 -0400

    970.1

--- tests/960_test.py
+++ tests/960_test.py
@@ -1,15 +1,15 @@
-import unittest
-import sys
-import re
-sys.path.append('..')
+# import unittest
+# import sys
+# import re
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        code = open("blackjack.py").read()
-        pattern = re.compile("try:\s+games_to_play = int\(input\(\"How many games do you want to play\?: \"\)\)\s+except:\s+print\(\"You must enter a number.\"\)")
-        found_pattern = pattern.search(code)
-        self.assertTrue(found_pattern, "Code should contain try-catch block")
-if __name__ == '__main__':
-    unittest.main() 
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         code = open("blackjack.py").read()
+#         pattern = re.compile("try:\s+games_to_play = int\(input\(\"How many games do you want to play\?: \"\)\)\s+except:\s+print\(\"You must enter a number.\"\)")
+#         found_pattern = pattern.search(code)
+#         self.assertTrue(found_pattern, "Code should contain try-catch block")
+# if __name__ == '__main__':
+#     unittest.main() 
---
+++ tests/970_test.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest import mock
+import sys
+sys.path.append('..')
+
+class PythonTest(unittest.TestCase):
+
+    def test_python(self):
+        code = open("blackjack.py").read()
+        code = code.replace('g = Game()', '        return locals()\ng = Game()')
+        with unittest.mock.patch('builtins.input', side_effect=[1, "f", 'h', 9]): # side_effects is the list of mocked inputs
+            exec(code)
+            g = locals()["Game"]()
+            local_variables = g.play()
+
+        self.assertEqual(local_variables['games_to_play'], 9)
+
+
+if __name__ == '__main__':
+    unittest.main() 
+
+
+
+
