b5b4d895be76d64fb4e54291c284e8c7b75c974d
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-24 16:04:52 -0400

    990.1

--- tests/980_test.py
+++ tests/980_test.py
@@ -1,23 +1,23 @@
-import unittest
-from unittest import mock
-import sys
-sys.path.append('..')
+# import unittest
+# from unittest import mock
+# import sys
+# sys.path.append('..')
 
-class PythonTest(unittest.TestCase):
+# class PythonTest(unittest.TestCase):
 
-    def test_python(self):
-        code = open("blackjack.py").read()
-        code = code.replace('g = Game()', '        return locals()\ng = Game()')
-        with unittest.mock.patch('builtins.input', side_effect=[1, "f", 'h', 9]): # side_effects is the list of mocked inputs
-            exec(code)
-            g = locals()["Game"]()
-            local_variables = g.play()
-        print(local_variables)
-        self.assertEqual(local_variables['game_number'], 9, "game_number should increment to be equal to games_to_play.")
+#     def test_python(self):
+#         code = open("blackjack.py").read()
+#         code = code.replace('g = Game()', '        return locals()\ng = Game()')
+#         with unittest.mock.patch('builtins.input', side_effect=[1, "f", 'h', 9]): # side_effects is the list of mocked inputs
+#             exec(code)
+#             g = locals()["Game"]()
+#             local_variables = g.play()
+#         print(local_variables)
+#         self.assertEqual(local_variables['game_number'], 9, "game_number should increment to be equal to games_to_play.")
 
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
 
 
 
---
+++ tests/990_test.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest import mock
+import sys
+sys.path.append('..')
+
+from blackjack import *
+
+class PythonTest(unittest.TestCase):
+
+    def test_python(self):
+        code = open("blackjack.py").read()
+        code = code.replace('g = Game()', '        return locals()\ng = Game()')
+        with unittest.mock.patch('builtins.input', side_effect=[1, "f", 'h', 1]): # side_effects is the list of mocked inputs
+            exec(code)
+            g = locals()["Game"]()
+            local_variables = g.play()
+        print(local_variables)
+
+        cards1 = [str(card) for card in local_variables['deck'].cards] 
+        cards2 = [str(card) for card in Deck().cards] 
+
+        self.assertTrue(isinstance(local_variables['deck'], Deck), "deck should be Deck class.")
+        self.assertNotEqual(cards1, cards2, "Cards in deck should be shuffled.")
+
+if __name__ == '__main__':
+    unittest.main() 
+
+
+
+
